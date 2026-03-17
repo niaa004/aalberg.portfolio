48f35e48d7d5f4333a2db63ef47b3a6c71dc63b9
Nicolai <146740957+niaa004@users.noreply.github.com>
2026-03-17 20:20:21 +0100

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,17 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
